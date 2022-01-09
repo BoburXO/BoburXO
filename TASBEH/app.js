17a503ae4c63e3af84d4b01a2749553ec9222849
BoburXO <97375635+BoburXO@users.noreply.github.com>
2022-01-09 10:17:02 +0500

    Add files via upload

---
+++ TASBEH/app.js
@@ -0,0 +1,28 @@
+let root = document.getElementById("root");
+let al = "Alhamdulillah";
+let ol = "Allohu akbar";
+let la = "La ilaha illallohu vahdahu la shariyka lah. Lahul mulku va lahul hamd. Va huva ala kulli shay in qodiyr"
+let h2 = document.getElementById("sub");
+let ekran = document.querySelector(".ekran");
+document.querySelector(".btn").addEventListener("click", function () {
+  console.log(+ekran.value + 1);
+  ekran.value = parseInt(ekran.value) + 1;
+
+  if (ekran.value == 33) {
+    h2.innerHTML = al;
+    console.log(al);
+  } else if (ekran.value == 66) {
+    h2.innerHTML = ol;
+    console.log(ol);
+  }else if (ekran.value == 99){
+      h2.innerHTML = la;
+      console.log(la);
+  }
+});
+
+document.querySelector(".nol").addEventListener("click", function () {
+  document.querySelector(".ekran").value = -1;
+});
+
+
+
